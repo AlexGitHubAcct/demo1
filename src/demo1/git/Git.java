b811c42e9cbdf4b14db1520eeb98625a4a532c0c
Alexp <Alexp@192.168.254.149>
2023-01-12 17:57:26 -0600

    Change 2

--- src/demo1/git/Git.java
+++ src/demo1/git/Git.java
@@ -1,8 +1,11 @@
 package demo1.git;
 
-public class Git {
-public static void main(String[] args) 
+public class Git 
+{
+	public static void main(String[] args) 
 	{
 		System.out.println("GIT");
+	
+		System.out.println("HUB");
 	}
 }
